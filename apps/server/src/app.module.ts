dc986243d16c9e7873426a9ee722f1d670be5f0b
Santiago Andres Quintero Rodriguez <santianqr@gmail.com>
2024-11-14 13:58:01 -0500

    Add Prisma integration with schema and service implementation

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TrpcModule } from './trpc/trpc.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), TrpcModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
